962e43878061bdfdf229f2efc3347ab40916b3f3
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2025-01-14 19:55:18 -0500

    name change

--- src/lib.rs
+++ src/lib.rs
@@ -3,50 +3,61 @@ pub struct Axios {
 }
 
 impl Axios {
-    pub fn configure(url: String) -> Axios {
-        let parsed_url = if url.ends_with("/") {
-            url.trim_end_matches("/").to_string()
-        } else {
-            url
-        };
+    // pub fn configure(url: Option<String>) -> Axios {
+    //     // let parsed_url = if url.ends_with("/") {
+    //     //     url.trim_end_matches("/").to_string()
+    //     // } else {
+    //     //     url
+    //     // };
+    //
+    //     let parsed_url = url.map(|u| {
+    //         if u.ends_with("/") {
+    //             u.trim_end_matches("/").to_string()
+    //         } else {
+    //             u
+    //         }
+    //     });
+    //
+    //     Axios {
+    //         // base_url: Some(parsed_url),
+    //         base_url: parsed_url,
+    //     }
+    // }
+    pub fn configure<U>(url: U) -> Self
+    where
+        U: Into<Option<String>>,
+    {
+        let parsed_url = url.into().map(|u| {
+            if u.ends_with("/") {
+                u.trim_end_matches("/").to_string()
+            } else {
+                u
+            }
+        });
 
         Axios {
-            base_url: Some(parsed_url),
+            base_url: parsed_url,
         }
     }
 
-    pub fn get(&self, endpoint: String) -> String {
-        let base_url = self.base_url.as_ref().expect("Base URL is not configured");
+    pub async fn get(&self, url: String) -> String {
+        if let Some(base_url) = &self.base_url {
+            // Trim leading slash from the endpoint if necessary
+            let parsed_endpoint = if url.starts_with("/") {
+                url.trim_start_matches("/").to_string()
+            } else {
+                url
+            };
 
-        let parsed_endpoint = if endpoint.starts_with("/") {
-            endpoint.trim_start_matches("/").to_string()
+            // Combine base URL with the endpoint
+            format!("{}/{}", base_url, parsed_endpoint)
         } else {
-            endpoint
-        };
-
-        format!("{}/{}", base_url, parsed_endpoint).to_string()
+            // If base_url is None, use the provided URL as-is
+            url
+        }
+        // do the request
     }
 }
-
-// enum Method {
-//     GET,
-//     POST,
-//     PUT,
-//     DELETE,
-// }
-//
-// struct Request {
-//     method: Method,
-//     headers: Option<(String, String)>,
-//     body: String, // JSON, XML w/ post or put
-//     url: String,  // is there a URL/Path type?
-// }
-//
-// struct Response {
-//     status_code: i32, // 200 OK, 404 Not Found, etc...
-//     headers: String,  // Content-Type: "application/json"
-//     body: String,     // JSON or HTML
-// }
 
 #[cfg(test)]
 mod tests {
@@ -55,7 +66,7 @@ mod tests {
     #[test]
     fn test_configure_url() {
         let result = Axios::configure("google.com".to_string());
-        assert_eq!(result.base_url, Some("google.com".to_string()));
+        assert_eq!(result.base_url, "google.com".to_string().into());
     }
 
     #[test]
@@ -64,11 +75,41 @@ mod tests {
         assert_eq!(result.base_url, Some("http://github.com".to_string()));
     }
 
-    #[test]
-    fn test_get_output() {
+    #[tokio::test]
+    async fn test_get_output() {
         let config = Axios::configure("http://github.com/".to_string());
-        let result = config.get("users".to_string());
+        let result = config.get("users".to_string()).await;
         let expected = "http://github.com/users".to_string();
         assert_eq!(result, expected)
     }
+
+    #[tokio::test]
+    async fn test_no_base_url() {
+        let config = Axios::configure(None);
+        let res = config.get("http://google.com".to_string()).await;
+        let expected = "http://google.com".to_string();
+        assert_eq!(res, expected)
+    }
 }
+
+// pub use Request {
+//
+//     pub enum Method {
+//         GET,
+//         POST,
+//         PUT,
+//         DELETE,
+//     }
+//
+//     pub struct Request {
+//         method: Method,
+//         headers: Option<(String, String)>,
+//         body: String, // JSON, XML w/ post or put
+//         url: String,  // is there a URL/Path type?
+//     }
+//
+//     pub struct Response {
+//         status_code: i32, // 200 OK, 404 Not Found, etc...
+//         headers: String,  // Content-Type: "application/json"
+//         body: String,     // JSON or HTML
+//     }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use fetcher::Axios;
+use echo_http::Axios;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let a = Axios::configure("github.com/".to_string());
-    let res = a.get("users".to_string());
-
+    let a_some = Axios::configure(Some("github.com/".to_string()));
+    let res = a_some.get("users".to_string()).await;
     println!("{}", res);
-    println!("main");
+
+    let a_none = Axios::configure(None);
+    let r = a_none.get("http://localhost:3000/api/dev/users/123".to_string()).await;
+    println!("{}", r);
 
     Ok(())
 }
